7a95e1d209fd308ca64ab0a839eb8135da709758
Md. Rakibul Islam <rakibul.islam@sslwireless.com>
2022-03-23 15:03:06 +0600

    turn off warning / error messages

--- u2b.php
+++ u2b.php
@@ -1,7 +1,7 @@
 <?php
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
+// ini_set('display_errors', '1');
+// ini_set('display_startup_errors', '1');
+// error_reporting(E_ALL);
 
 # Get the full list of mobile numbers
 $fo = file_get_contents('data.csv');
